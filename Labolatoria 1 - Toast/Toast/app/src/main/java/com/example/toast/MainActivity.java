3962d5c7ac9a53e9ca757df840ab1fbd90329f7c
JWORD200 <jan.tomczak@zsegw.pl>
2023-09-20 23:16:52 +0200

    Zadanie 1

---
+++ Labolatoria 1 - Toast/Toast/app/src/main/java/com/example/toast/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Imię: Jan - Nazwisko: Tomczak", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
